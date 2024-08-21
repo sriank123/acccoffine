8c475341178bdac135fcc60301080aead08465fe
sriank123 <141177387+sriank123@users.noreply.github.com>
2024-08-21 20:49:51 +0530

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,31 +2,27 @@ import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home/Home';
-import Cart from './pages/Cart/Cart'; // Import Cart component
-import PlaceOrder from './pages/PlaceOrder/PlaceOrder'; // Import PlaceOrder component
-import Footer from './components/Footer/Footer'; // Import Footer component
+import Cart from './pages/Cart/Cart';
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
+import Footer from './components/Footer/Footer';
 import LoginPopup from './components/LoginPopup/LoginPopup';
-import About from './components/about/about'
+import About from './components/about/about';
 const App = () => {
-
-  const[showLogin,setShowLogin]=useState(false)
+  const [showLogin, setShowLogin] = useState(false);
   return (
     <>
-
-    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
-    <div className='app'>
-      <Navbar setShowLogin={setShowLogin}/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/cart' element={<Cart/>}/>
-        <Route path='/order' element={<PlaceOrder/>}/>
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </div>
-    <Footer/>
+      {showLogin ? <LoginPopup setShowLogin={setShowLogin}/> : <></>}
+      <div className='app'>
+        <Navbar setShowLogin={setShowLogin}/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/order' element={<PlaceOrder/>}/>
+          <Route path="/about" element={<About/>}/>
+        </Routes>
+      </div>
+      <Footer/>
     </>
-    
   );
-}
-
+};
 export default App;
